824cf3b92a796340d45d79cd0d28c54b51c7bc0a
Junaid <qjunaid623@gmail.com>
2020-07-18 20:01:05 +0500

    Updated Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,13 @@ import React from "react";
 function Home() {
   return (
     <div>
-      <h1>Hello from Home!</h1>
+      <div className="banner-image"></div>
+      <div>
+        <span className="tagline">JUST DO IT</span>
+        <div className="tagline-para">Nike has been your companion in happiness, both big and small, for over 50 years. Our roots are firmly grounded in Pakistan and with its people.</div>
+      </div>
     </div>
-  );  
+  );
 }
 
 export default Home;
