b392b22510d5e73a6eaa598096574c49fcac6c34
Junaid <qjunaid623@gmail.com>
2020-07-19 14:26:33 +0500

    Updated site

--- src/components/About.js
+++ src/components/About.js
@@ -3,8 +3,25 @@ import React from "react";
 function About() {
   return (
     <div>
-      <h1>Hello from About!</h1>
-      
+      <div className="banner-image-about">
+        <div className="about-nike">
+          <h2 className="about-nike-text">OUR MISSION</h2>
+          <p>
+            "BRING INSPIRATION AND INNOVATION TO EVERY ATHLETE* IN THE WORLD"
+          </p>
+        </div>
+      </div>
+      <div className="about-nike-part">
+        <h2 className="about-nike-heading">About Nike</h2>
+        <p className='about-nike-para'>
+          Our mission is what drives us to do everything possible to expand
+          human potential. We do that by creating groundbreaking sport
+          innovations, by making our products more sustainably, by building a
+          creative and diverse global team and by making a positive impact in
+          communities where we live and work. Based in Beaverton, Oregon, NIKE,
+          Inc. includes the Nike, Converse, and Jordan brands.
+        </p>
+      </div>
     </div>
   );
 }
