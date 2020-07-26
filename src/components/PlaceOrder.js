63a1ee8c78c9eb1afbbe25faeda0c9e85724e324
Junaid <qjunaid623@gmail.com>
2020-07-26 22:37:04 +0500

    Fixed some bugs

--- src/components/PlaceOrder.js
+++ src/components/PlaceOrder.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import '../App.css';
+import React, { useContext, useEffect } from "react";
+import "../App.css";
+import { CartContext } from "../CartContext";
 
-function PlaceOrder(){
-    return(
-        <div className='placeOrder-container'>
-            <h2 className='thankyou'>Thank you for your order</h2>
-            <p>Your order number is <strong>#2001539</strong>. We have emailed your order confirmation, and will send you an update when your order has shipped.</p><br/>
-            <h2 className='dont-worry'>Don't worry it's just a template !</h2><br/>
-        </div>
-    )
+function PlaceOrder() {
+  const [cart, setCart] = useContext(CartContext);
+  useEffect(() => {
+    setCart([]);
+  }, []);
+
+  return (
+    <div className="placeOrder-container">
+      <h2 className="thankyou">Thank you for your order</h2>
+      <p>
+        Your order number is <strong>#2001539</strong>. We have emailed your
+        order confirmation, and will send you an update when your order has
+        shipped.
+      </p>
+      <br />
+      <h2 className="dont-worry">Don't worry it's just a template !</h2>
+      <br />
+    </div>
+  );
 }
 
 export default PlaceOrder;
