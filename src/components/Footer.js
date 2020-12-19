d77e50cd251f409cb0b58042732782a6399cd76c
developer-junaid <qjunaid623@gmail.com>
2020-12-19 09:58:50 +0500

    Footer Bug Resolved, installed redux, react-redux, redux-toolkit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,58 +3,54 @@ import "./../App.css";
 
 function Footer() {
   return (
-    <div className="footer-container">
-      
-      <div className="page-wrapper"></div>
-      <hr />
-      <footer>
-        <div className="footer">
-          <div className="footer-content">
-            <div className="footer-section about">
-              
-              <p className="footer-para">
-               Developer: <span className='my-name'> Junaid Qureshi</span>
-              </p>
-             
-              <div className="contact">
-                
-              </div>
-              <div className="socials">
-                <a
-                  href="https://www.facebook.com/profile.php?id=100041078220896"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <i className="fab fa-facebook"></i>
-                </a>
-                <a
-                  href="https://www.linkedin.com/in/junaid-qureshi-34a180194/"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  <i className="fab fa-linkedin"></i>
-                </a>
-                <a
-                  href="https://twitter.com/Junaid084869/"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  <i className="fab fa-twitter"></i>
-                </a>
-                <a
-                  href="https://github.com/developer-junaid"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  <i className="fab fa-github"></i>
-                </a>
-              </div>
-              <p className='copyright'>Copyright&nbsp;&copy;&nbsp;Nike Shoes Template</p><br/>
+    <footer>
+      <div className="wrapper"></div>
+      <div className="footer">
+        <div className="footer-content">
+          <div className="footer-section about">
+            <p className="footer-para">
+              Developer: <span className="my-name"> Junaid Qureshi</span>
+            </p>
+
+            <div className="contact"></div>
+            <div className="socials">
+              <a
+                href="https://www.facebook.com/profile.php?id=100041078220896"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fab fa-facebook"></i>
+              </a>
+              <a
+                href="https://www.linkedin.com/in/junaid-qureshi-34a180194/"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <i className="fab fa-linkedin"></i>
+              </a>
+              <a
+                href="https://twitter.com/Junaid084869/"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <i className="fab fa-twitter"></i>
+              </a>
+              <a
+                href="https://github.com/developer-junaid"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <i className="fab fa-github"></i>
+              </a>
             </div>
+            <p className="copyright">
+              Copyright&nbsp;&copy;&nbsp;Nike Shoes Template
+            </p>
+            <br />
           </div>
         </div>
-      </footer>
-    </div>
+      </div>
+    </footer>
   );
 }
 
